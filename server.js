c7ac7a5ad5785e00b7947e8a91236948a3b6ef05
imarikacos <imarikacos@gmail.com>
2025-08-14 14:48:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://swiftloanske.netlify.app'
+  origin: 'https://loanswiftke.netlify.app'
 }));
 
 function formatPhone(phone) {
