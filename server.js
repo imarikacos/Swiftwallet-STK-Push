a28bb5b0d41419d2c7216e05b7491f19b1c696ce
imarikacos <imarikacos@gmail.com>
2025-08-14 08:36:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://whimsical-marigold-750b55.netlify.app'
+  origin: 'https://swiftloanske.netlify.app'
 }));
 
 function formatPhone(phone) {
