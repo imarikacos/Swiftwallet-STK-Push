0e8ab64ac9f86a45a05c2acbbe413c8be21b4946
imarikacos <imarikacos@gmail.com>
2025-08-29 00:42:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(cors({
   origin: '
-    https://swiftloanke-com.onrender.com'
+    'https://swiftloanke-com.onrender.com'
 }));
 
 function formatPhone(phone) {
